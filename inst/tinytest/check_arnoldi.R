ec921df37d84eba923cc3debac5ce5b59df6c034
Nikolas Kuschnig <nkuschni@wu.ac.at>
2021-05-18 18:11:36 +0200

    Patch some more and new mem leaks

--- inst/tinytest/check_arnoldi.R
+++ inst/tinytest/check_arnoldi.R
@@ -1,7 +1,7 @@
 
 Rcpp::sourceCpp("src/lanczos.cpp")
 
-N <- 100
+N <- 1000
 z <- rnorm(N)
 x <- sanic::sparsify(y <- matrix(rnorm(N^2), N))
 x <- sanic::sparsify(y <- crossprod(matrix(rnorm(N^2), N)))
@@ -26,6 +26,23 @@ mb(
 
 mgcv::slanczos(x)
 
-eigen(x, symmetric = TRUE)$values
+sort(eigen(x, symmetric = TRUE)$values)
+
+sort(hessenberg(x)$T)
+
+sort(arnoldi(x, z)$T)
 
 arnoldi(x, z, iter = 10)$T
+
+a1 <- arnoldi(x)
+a2 <- arnoldi(x, iter = N / 2)
+h1 <- hessenberg(x)
+t1 <- tridiagonal(x)
+
+plot(sort(a1$T, TRUE))
+points(sort(a2$T, TRUE), col = "#800000")
+points(sort(h1$T, TRUE), col = "#008080")
+points(sort(t1$E, TRUE), col = "#000080")
+
+mb(arnoldi(x), arnoldi(x, iter = N / 2), hessenberg(x), tridiagonal(x),
+  times = 2)
--- src/lanczos.cpp
+++ src/lanczos.cpp
@@ -35,11 +35,11 @@
 //   return v;
 // }
 
+  // const Eigen::Map <Eigen::VectorXd> b,
 
 // [[Rcpp::export]]
 Rcpp::List arnoldi(
   const Eigen::MappedSparseMatrix<double> a,
-  const Eigen::Map <Eigen::VectorXd> b,
   double tol = 0, int iter = 0) {
 
   if(!iter) {
@@ -50,30 +50,32 @@ Rcpp::List arnoldi(
   }
 
   Eigen::MatrixXd q = Eigen::MatrixXd::Zero(a.rows(), iter);
-  Eigen::MatrixXd h = Eigen::MatrixXd::Zero(iter, a.rows());
+  Eigen::MatrixXd h = Eigen::MatrixXd::Zero(iter + 1, iter);
   Eigen::VectorXd v(a.rows());
-  q.col(0) = b.normalized();
-  // Eigen::VectorXd::Random(a.rows()).normalized();
+  // q.col(0) = b.normalized();
+  q.col(0) = Eigen::VectorXd::Random(a.rows()).normalized();
 
-  for(int i = 0; i <= iter; i++) {
-    // Rcpp::Rcout << "i" << i << "\t";
+  for(int i = 0; i < iter; i++) {
+    // Rcpp::Rcout << "i=" << i << "\t";
     v = a * q.col(i);
     for(int j = 0; j <= i; j++) {
-      // Rcpp::Rcout << "j" << j << "\t";
+      // Rcpp::Rcout << "j=" << j << "\t";
       h(j, i) = q.col(j).dot(v);
       v = v - h(j, i) * q.col(j);
     }
-    if(i < iter) {
-      h(i + 1, i) = v.norm();
-      if(h(i + 1, i) >= tol) {
-        q.col(i + 1) = v / h(i + 1, i);
-      } else {
-        goto exit;
-      }
+    h(i + 1, i) = v.norm();
+    if(h(i + 1, i) < tol) {
+      // Rcpp::Rcout << "Tolerance";
+      goto exit;
+    } else if(i < iter - 1) {
+      // Rcpp::Rcout << "Orthogonalisation";
+      q.col(i + 1) = v / h(i + 1, i);
     }
+    // Rcpp::Rcout << "\n";
   }
 
   exit:
+  // Rcpp::Rcout << "Exiting\n";
   Eigen::RealSchur <Eigen::MatrixXd> schur;
   schur.computeFromHessenberg(h.topLeftCorner(iter, iter), q.topLeftCorner(iter, iter), false);
   Eigen::MatrixXd t = schur.matrixT();
@@ -96,4 +98,21 @@ Rcpp::List hessenberg(
   Eigen::MatrixXd t = schur.matrixT();
 
   return Rcpp::List::create(Rcpp::Named("Q") = q, Rcpp::Named("H") = h, Rcpp::Named("T") = t.diagonal());
+}
+
+
+// [[Rcpp::export]]
+Rcpp::List tridiagonal(
+  const Eigen::MappedSparseMatrix<double> a) {
+
+  Eigen::Tridiagonalization <Eigen::MatrixXd> tri;
+  tri.compute(a);
+  Eigen::MatrixXd d = tri.diagonal();
+  Eigen::MatrixXd s = tri.subDiagonal();
+
+  Eigen::SelfAdjointEigenSolver <Eigen::MatrixXd> trisolver;
+  trisolver.computeFromTridiagonal(d, s, false);
+  Eigen::VectorXd ev = trisolver.eigenvalues();
+
+  return Rcpp::List::create(Rcpp::Named("D") = d, Rcpp::Named("S") = s, Rcpp::Named("E") = ev);
 }
